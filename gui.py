9c40e509efb26e9e27313c2d4df516719ae34b08
dogusural <dogusural@gmail.com>
2021-01-19 15:28:55 +0100

    initial commit

---
+++ gui.py
@@ -0,0 +1,76 @@
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from yield_calculator import yield_calculator as calc
+
+
+class yield_gui:
+    def __init__(self):
+        self.fields = { ('scale', 'USD Amount', 50000 , 5, 0), ('scale', 'Harvesting Period (days)',360, 1, 1), ('scale', 'Duration in Years',10, 1, 1) , ('entry', '% APY on stable') , ('entry', '% APY on $CAKE') }
+        self.entries = {}
+        self.root= None
+        self.calculator = None
+        self.chart = None
+        self.result = None
+
+    def makeform(self):
+        scl = 0
+        for field in self.fields:
+            row = tk.Frame(self.root)
+            lab = tk.Label(row, width=22, text=field[1]+": ", anchor='w')
+            if field[0] == 'scale':
+                scl = tk.Scale(row, from_=0, to=field[2], resolution=field[3], orient=tk.HORIZONTAL)
+                scl.set(field[4])
+            else:
+                scl = tk.Entry(row)
+                if "stable" in field[1]:
+                    scl.insert(0, "28.77")
+                else:
+                    scl.insert(0, "229.35")
+
+            row.pack(side=tk.TOP, 
+                    fill=tk.X, 
+                    padx=5, 
+                    pady=5)
+            lab.pack(side=tk.LEFT)
+            scl.pack(side=tk.RIGHT, 
+                    expand=tk.YES, 
+                    fill=tk.X)
+            self.entries[field[1]] = scl
+
+    def get_values(self):
+        if self.chart is not None :
+            self.chart.get_tk_widget().pack_forget()
+        if self.result is None :
+            self.result = tk.Label(self.root,text="0")
+            self.result.pack()
+            print(type(self.result))
+
+        amount = int(self.entries['USD Amount'].get())
+        period = int(self.entries['Harvesting Period (days)'].get())
+        duration = int(self.entries['Duration in Years'].get())
+        stable_apy = float(self.entries['% APY on stable'].get())
+        cake_apy = float(self.entries['% APY on $CAKE'].get())
+       
+        self.calculator = calc(amount,period,stable_apy,cake_apy,duration)
+        data = self.calculator.calculate()
+        fig = plt.Figure(figsize=(5,4),dpi=100)
+        print(data[0][-1])
+        fig.add_subplot(111).plot(data[1],data[0],marker='')
+        self.result.config(text=str(data[0][-1]))
+        self.chart = FigureCanvasTkAgg(fig,self.root)
+        self.chart.get_tk_widget().pack()
+        
+
+
+    def run(self):
+        self.root = tk.Tk()
+        self.root.title('Pancake')
+        self.root.geometry("700x700")
+        self.makeform()
+        tk.Button(self.root, text='Calculate', command=self.get_values).pack(pady=8)
+        self.root.mainloop()
+
+
+gdp = yield_gui()
+gdp.run()
---
+++ yield_calculator.py
@@ -0,0 +1,42 @@
+
+class yield_calculator:
+
+    days_in_year = 365
+    total_harvest_fee = 0.5
+
+    def __init__(self, stable_amount, harvest_period, apy_stable, apy_cake, harvest_year=1):
+        self.stable_amount = stable_amount
+        self.harvest_period = harvest_period
+        self.harvest_year=harvest_year
+        self.apy_stable = apy_stable/100
+        self.apy_cake = apy_cake/100
+        self.epoch = yield_calculator.days_in_year / harvest_period * harvest_year
+
+    def calculate(self):
+        cake_history = []
+        epoch_history = []
+        epoch_count = 1
+        cake_amount = 0
+        while self.epoch > 0:
+
+            if self.epoch < 1 :
+                stable_yield = ( self.stable_amount * self.apy_stable / yield_calculator.days_in_year * self.harvest_period * self.epoch) 
+                cake_yield = ( cake_amount * self.apy_cake / yield_calculator.days_in_year * self.harvest_period * self.epoch) 
+                cake_amount = cake_amount + cake_yield + stable_yield - yield_calculator.total_harvest_fee
+                epoch_history.append(int(epoch_count * self.harvest_period - self.harvest_period * self.epoch))
+            else :
+                stable_yield = ( self.stable_amount * self.apy_stable / yield_calculator.days_in_year * self.harvest_period) 
+                cake_yield = ( cake_amount * self.apy_cake / yield_calculator.days_in_year * self.harvest_period) 
+                cake_amount = cake_amount + cake_yield + stable_yield - yield_calculator.total_harvest_fee
+                epoch_history.append(epoch_count*self.harvest_period)
+
+            cake_history.append(cake_amount)
+            epoch_count += 1
+            self.epoch = self.epoch - 1
+        return(cake_history,epoch_history)
+
+
+
+
+
+
